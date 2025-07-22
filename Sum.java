26d6a6fe76bdf1ec1a40e47e89bfa3f070ccad6f
anughna003 <gayathrianughna1904@gmail.com>
2025-07-22 22:32:19 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,11 @@
+class Sum{
+public static void main(String[] args){
+int i;
+int sum=0;
+for(i=1;i<=10;i++){
+sum+=i;
+}
+System.out.println("Sum of first 10 numbers is:"+sum);
+System.out.println("Avg of first 10 numbers is:"+(sum/10));
+}
+}
